e294de08f98065c9ed34561cd0a056ee7f265bd3
Heribert <crossbrand96@gmail.com>
2023-12-10 02:38:58 +0100

    modify ui and make app functional

---
+++ assets/scripts/palindrome.js
@@ -0,0 +1,27 @@
+// Get the html to be used and save them in their various variables
+
+const inputElement = document.querySelector('.js-input')
+const buttonElement = document.querySelector('.js-button')
+const outputElement = document.querySelector('.js-output')
+
+const palindromeCheck = () => {
+   // insist on the user input string case to be compared
+  const take = inputElement.value.toLowerCase()
+
+  const stretch = take.length
+
+  // Loop for matches during simultaenous iterations from both ends heading towards the 'middle' of the inputed string
+  for (let i = 0; i < stretch / 2; i++) {
+    if (take[i] !== take[stretch - 1 - i]) {
+     return outputElement.value =  `Not a Palindrome!`
+    }
+  }
+  return outputElement.value =  `Palindrome!`
+}
+
+// RESET APP
+const clearScreen = () => {
+  outputElement.value = ''
+  inputElement.value = ''
+}
+ 
